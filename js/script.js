61e0c2b38bbd16b32473488cfd5f435d92bb06d1
Jared Gavigan <jagavigan80@gmail.com>
2021-09-23 00:20:48 -0400

    trash btn

--- js/script.js
+++ js/script.js
@@ -9,6 +9,27 @@ todoButton.addEventListener('click', addTodo);
 //Functions
 
 function addTodo(event) {
-    console.log('hello');
+    //prevent form from submitting
+    event.preventDefault();
+    //Todo DIv
+    let todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+    //Create Li
+    let newTodo = document.createElement('li');
+    newTodo.innerText = 'todoInput.value';
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+    //Check Mark Button
+    let completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+     //Check Trash Button
+     let trashButton = document.createElement('button');
+     trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+     trashButton.classList.add("trash-btn");
+     todoDiv.appendChild(trashButton);
+     //Append To List
+     todoList.appendChild(todoDiv);
 }
 
